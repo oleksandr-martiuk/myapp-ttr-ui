6ada2a56a3926b7f032b7ff2cc4d5568e30b4375
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2019-12-19 08:16:58 +0200

    upgraded DESIGN of HIDEN button

--- js/components/todolist.js
+++ js/components/todolist.js
@@ -18,7 +18,7 @@ addInput.addEventListener("keyup", async (event) => {
       await renderAllTasks();
 
       cleanAddInput();
-      mainWin.minimize(); // TODO: **USER** mode
+      hideWindow(); // TODO: **USER** mode
    }
 });
 
