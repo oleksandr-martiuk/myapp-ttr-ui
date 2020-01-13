dc2de35c7c71f0349cead48f567df0ea5dab60fb
Oleksandr Martiuk <oleksandr.martiuk@gmail.com>
2020-01-13 23:55:13 +0200

    upgraded styles for Menu window

--- src/shared/components/Menu/Menu.tsx
+++ src/shared/components/Menu/Menu.tsx
@@ -2,61 +2,61 @@ import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {createSession, getLastSession} from "../../../components/TimeReports/redux/Session/session.services";
 import {ISessionOptions} from "../../services/requests/session";
-import {styled} from "@material-ui/core";
 import {IMenuItem} from "./types/menu-item";
 import {MENU_ITEMS} from "../../constants";
 import {ITime} from "../../../components/TimeReports/redux/Time/types/time";
 import {updateTimeAction} from "../../../components/TimeReports/redux/Time/time.actions";
+import {withStyles} from "@material-ui/styles";
+import {Theme} from "@material-ui/core/styles";
 
 const menuBtnImg = require('../../images/menu_btn.png');
 
-const MenuButton = styled('div')({
-   backgroundImage: `url(${menuBtnImg})`,
-   position: 'fixed',
-   backgroundPosition: '0 -1px',
-   top: '10px',
-   left: '10px',
-   zIndex: 1,
-   border: '1px solid #46a882',
-   borderRadius: '3px',
-   backgroundRepeat: 'no-repeat',
-   color: '#FFFFFF',
-   width: '39px',
-   height: '38px',
-   cursor: 'pointer',
-   alignContent: 'center',
-   '&:hover': {
-      backgroundPosition: '0 -39.6px',
-      boxShadow: '0 0 10px #60daaa',
-   },
-   '&:active': {
-      backgroundPosition: '0 -78.5px',
-      boxShadow: '0 0 10px #46a882',
-   }
-});
-
-const MenuWindow = styled('div')({
-   position: 'fixed',
-   top: '55px',
-   left: '10px',
-   zIndex: 100,
-   border: '0.5px solid #46a882',
-   borderRadius: '3px',
-   backgroundColor: '#000000',
-   color: '#FFFFFF',
-   width: '200px',
-   height: '105px',
-   alignContent: 'center',
-   padding: '10px',
-});
-
-const MenuItem = styled('div')({
-   color: '#46a882',
-   padding: '8px',
-   cursor: 'pointer',
-   "&:hover": {
+const styles = (theme: Theme): any => ({
+   menuButton: {
+      backgroundImage: `url(${menuBtnImg})`,
+      position: 'fixed',
+      backgroundPosition: '0 -1px',
+      top: '10px',
+      left: '10px',
+      zIndex: 1,
+      border: '1px solid #46a882',
+      borderRadius: '3px',
+      backgroundRepeat: 'no-repeat',
       color: '#FFFFFF',
-      textShadow: '0 0 10px #46a882',
+      width: '39px',
+      height: '38px',
+      cursor: 'pointer',
+      alignContent: 'center',
+      '&:hover': {
+         backgroundPosition: '0 -39.6px',
+         boxShadow: '0 0 10px #60daaa',
+      },
+      '&:active': {
+         backgroundPosition: '0 -78.5px',
+         boxShadow: '0 0 10px #46a882',
+      }
+   },
+   menuWindow: {
+      position: 'fixed',
+      top: '55px',
+      left: '10px',
+      zIndex: 100,
+      border: '0.5px solid #46a882',
+      borderRadius: '3px',
+      backgroundColor: '#000000',
+      color: '#FFFFFF',
+      width: '200px',
+      alignContent: 'center',
+      padding: '10px',
+   },
+   menuItem:{
+      color: '#46a882',
+      padding: '8px',
+      cursor: 'pointer',
+      "&:hover": {
+         color: '#FFFFFF',
+         textShadow: '0 0 10px #46a882',
+      }
    }
 });
 
@@ -70,30 +70,32 @@ class Menu extends Component<any, any> {
    }
 
    public render() {
+      const { classes } = this.props;
+      const menuWindowHeight = (this.props.session && this.props.session.isRunning) ? "70px" : "105px";
+
       return (
          <>
-            <MenuButton onClick={this.handleClick}/>
+            <div className={classes.menuButton} onClick={this.handleWindowClick}/>
             {(this.state.onWindowShow)
-               ? <MenuWindow>
+               ? <div className={classes.menuWindow} style={{height: menuWindowHeight}}>
                      {this.state.items.map((item: IMenuItem, index: number) => {
                         if (item.key === 'createSession' && this.props.session.isRunning) {
-                           console.log('MenuWindow: ', MenuWindow); // TODO: remove such log
                            return null;
                         }
                         return (
-                           <MenuItem key={item.key} onClick={() => this.handleItemClick(item.key)}>
+                           <div className={classes.menuItem} key={item.key} onClick={() => this.handleItemClick(item.key)}>
                               {item.value}
-                           </MenuItem>
+                           </div>
                         );
                      })}
-                 </MenuWindow>
+                 </div>
                : null
             }
          </>
       );
    }
 
-   private handleClick = () => {
+   private handleWindowClick = () => {
       this.setState({ onWindowShow: !this.state.onWindowShow });
    };
 
@@ -124,4 +126,6 @@ const mapDispatchToProps = (dispatch: any) => ({
 export default connect (
    mapStateToProps,
    mapDispatchToProps
-)(Menu);
+)(
+   withStyles(styles, { withTheme: true })(Menu)
+);
